268575e45547f0e50b7c473ce2e6b58088138e71
SS56Bo <sohamsarkar560@gmail.com>
2024-11-09 11:39:50 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ class Game:
     def __init__(self):
         py.init()
         self.screen = py.display.set_mode(RES)
+        self.clock = py.time.Clock()
+
+    def new_game(self):
+        pass
+
+    def update_game(self):
+        py.display.flip()
+        self.clock.tick(FPS)
