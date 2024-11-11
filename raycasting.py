0e8ee8c492db0a191373929af54ab1fc7ad119e8
SS56Bo <sohamsarkar560@gmail.com>
2024-11-11 20:34:26 +0530

    Added raycasting

--- raycasting.py
+++ raycasting.py
@@ -6,5 +6,11 @@ class RayCasting:
     def __init__(self, game):
         self.game = game
 
-    def ray_cast():
-        pass
+    def ray_cast(self):
+        ray_angle = self.game.player.angle - HALF_FOV + 0.001
+        print(ray_angle)
+        for ray in range(NUM_RAYS):
+            ray_angle+=DELTA_ANGLE
+
+    def update_ray(self):
+        self.ray_cast()
